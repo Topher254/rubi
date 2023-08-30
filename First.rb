5faea6f57f59b4cc715f25be842e60c5f596c83b
Topher254 <raphaelsarota@gmail.com>
2023-08-30 11:15:18 +0300

    instance variables

--- First.rb
+++ First.rb
@@ -1,6 +1,7 @@
 
 
 class Says_hello
+    # instance method
     def Saays_hello
         puts 'hi buddy'
     end
@@ -8,3 +9,6 @@ end
 
 obj1 = Says_hello.new
 obj1.Saays_hello
+# # objects
+# class methods
+
---
+++ Student.rb
@@ -0,0 +1,22 @@
+# using instance variables
+class Student
+    def initialize(name,email,contact)
+        @name = name
+        @email = email
+        @contact = contact #instance varioable
+
+    end
+    def printa
+        puts  @name
+    end
+    def printa_email
+        puts @email
+    end
+    def printa_contacts
+        puts @contact
+    end
+end
+Studen1 = Student.new('sarota','raphaelsaROTA@GMAIL.COM','+254796871876')
+Studen1.printa
+Studen1.printa_email
+Studen1.printa_contacts
